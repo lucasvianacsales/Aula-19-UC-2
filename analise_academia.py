3fec7b15eb9249164f265da60e33137a85b6bf24
Lucas Sales <100038556+lucasvianacsales@users.noreply.github.com>
2026-01-09 19:26:56 -0300

    Update analise_academia.py

--- analise_academia.py
+++ analise_academia.py
@@ -7,13 +7,43 @@ lista_alunos = dados["alunos"]
 
 media_idade = 0
 total_idade = 0
+total_frequencia = 0
 total_mensalidade = 0
+quantidade_aluno_plano = {}
+frequencia_total_plano = {}
 
 for aluno in lista_alunos:
     total_idade += aluno["idade"]
     total_mensalidade += aluno["mensalidade"]
 
+    plano = aluno["plano"]
+    frequencia = aluno["frequencia_mensal"]
+    
+    if plano in frequencia_total_plano:
+        frequencia_total_plano[plano] += frequencia
+    else:
+        frequencia_total_plano[plano] = frequencia
+    
+    if plano in quantidade_aluno_plano:
+        quantidade_aluno_plano[plano] += 1
+    else:
+        quantidade_aluno_plano[plano] = 1
+    
+    total_frequencia += frequencia
 media_idade = total_idade/len(lista_alunos)
 
-print(media_idade)
+# 2
 print(total_mensalidade)
+# 3
+print(media_idade)
+# 4
+print(frequencia_total_plano)
+# 5
+print(quantidade_aluno_plano)
+# 6
+print(total_frequencia)
+
+for plano in frequencia_total_plano:
+    porcentagem = (frequencia_total_plano[plano]/total_frequencia)*100
+
+    print(plano, porcentagem)
